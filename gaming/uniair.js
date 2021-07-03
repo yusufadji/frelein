4fbe82e399540f9ff6ab32ee09dbd77f437a9379
Yusuf Bhaskara Adji <admin@frelein.my.id>
2021-07-03 23:09:04 +0700

    added function if document ready

--- gaming/uniair.js
+++ gaming/uniair.js
@@ -1,11 +1,16 @@
-var 7936882821 = new Vue({
-  el: '#7936882821',
-  data: {
-    member: 'Kato Shiho',
-    rarity: 'SSR',
-    color: 'PURPLE',
-    centerSkill: 'Skill activation rate 2.25 times.',
-    skillType: 'Combo Support',
-    skill: '22% chance to continue combo every 9 seconds for 8 seconds on GOOD, BAD, or MISS.'
-  }
-})
+function ready(f) {
+  /in/.test(document.readyState) ? setTimeout('ready(' + f + ')', 9) : f();
+}
+ready(function() {
+  var uniair = new Vue({
+    el: '#uniair-7936882821',
+    data: {
+      member: 'Kato Shiho',
+      rarity: 'SSR',
+      color: 'PURPLE',
+      centerSkill: 'Skill activation rate 2.25 times.',
+      skillType: 'Combo Support',
+      skill: '22% chance to continue combo every 9 seconds for 8 seconds on GOOD, BAD, or MISS.'
+    }
+  })
+});
