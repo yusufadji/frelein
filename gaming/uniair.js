2fb8211d6acdba5b9d829d07924ace21695fab44
Yusuf Bhaskara Adji <admin@frelein.my.id>
2021-07-03 22:44:23 +0700

    add information katoshi yuniea matsuri

---
+++ gaming/uniair.js
@@ -0,0 +1,11 @@
+var 7936882821 = new Vue({
+  el: '#7936882821',
+  data: {
+    member: 'Kato Shiho',
+    rarity: 'SSR',
+    color: 'PURPLE',
+    centerSkill: 'Skill activation rate 2.25 times.',
+    skillType: 'Combo Support',
+    skill: '22% chance to continue combo every 9 seconds for 8 seconds on GOOD, BAD, or MISS.',
+  }
+})
