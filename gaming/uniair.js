00ab006e3b41e93b709bae4b6d611237296c2449
Yusuf Bhaskara Adji <admin@frelein.my.id>
2021-07-06 17:22:17 +0700

    edit to become variable storage

--- gaming/uniair.js
+++ gaming/uniair.js
@@ -1,16 +1,8 @@
-function ready(f) {
-  /in/.test(document.readyState) ? setTimeout('ready(' + f + ')', 9) : f();
+const u7936882821 = {
+  member: 'Kato Shiho',
+  rarity: 'SSR',
+  color: 'PURPLE',
+  centerSkill: 'Skill activation rate 2.25 times.',
+  skillType: 'Combo Support',
+  skill: '22% chance to continue combo every 9 seconds for 8 seconds on GOOD, BAD, or MISS.'
 }
-ready(function() {
-  var uniair = new Vue({
-    el: '#uniair-7936882821',
-    data: {
-      member: 'Kato Shiho',
-      rarity: 'SSR',
-      color: 'PURPLE',
-      centerSkill: 'Skill activation rate 2.25 times.',
-      skillType: 'Combo Support',
-      skill: '22% chance to continue combo every 9 seconds for 8 seconds on GOOD, BAD, or MISS.'
-    }
-  })
-});
